9f8ee57e579788f1a6d6071c0b6f3f6c621ac3d8
Jaimini <jaiminiraval123@gmail.com>
2024-06-05 14:34:06 +0530

    Create image slider

---
+++ task1/script.js
@@ -0,0 +1,6 @@
+
+ const slide = document.querySelector('.slides');
+ const img = document.querySelectorAll('.image-slider input');
+ const prev = document.querySelector('.prev');
+ const next = document.querySelector('.next');
+ let images = []; 
